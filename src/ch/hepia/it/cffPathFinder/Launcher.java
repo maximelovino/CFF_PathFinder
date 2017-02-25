91fa30b3631bd2753b4f1d75632fff4ccc23c4a9
Lovino Maxime <maximelovino@gmail.com>
2017-02-25 22:08:37 +0100

    first commit

---
+++ src/ch/hepia/it/cffPathFinder/Launcher.java
@@ -0,0 +1,7 @@
+package ch.hepia.it.cffPathFinder;
+
+public class Launcher {
+	public static void main (String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ src/ch/hepia/it/cffPathFinder/data/Parser.java
@@ -0,0 +1,4 @@
+package ch.hepia.it.cffPathFinder.data;
+
+public class Parser {
+}
