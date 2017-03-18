25bc9bdd838e9576f3e766ff5fb5aeca935294aa
Osoldier <thomas.ibanez33@gmail.com>
2017-03-18 15:58:48 +0100

    done dijkstra

--- src/ch/hepia/it/cffPathFinder/backend/Dijkstra.java
+++ src/ch/hepia/it/cffPathFinder/backend/Dijkstra.java
@@ -1,8 +1,15 @@
 package ch.hepia.it.cffPathFinder.backend;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Dijkstra implements PathFinder {
 	private static Dijkstra instance = new Dijkstra();
-
+	private Map<Vertex, Integer> distances;
+	private Map<Vertex, Vertex> precendence;
+	
 	private Dijkstra () {
 	}
 
@@ -11,6 +18,39 @@ public class Dijkstra implements PathFinder {
 	}
 
 	public Path shortestPath (Graph g, Vertex v1, Vertex v2) {
-		return null;
+		distances = new HashMap<>();
+		precendence = new HashMap<>();
+		Queue<Vertex> queue = new PriorityQueue<>((Vertex o1, Vertex o2)->Integer.compare(distances.get(o1), distances.get(o2)));
+		for (Vertex v : g.getVertices()) {
+			distances.put(v, Integer.MAX_VALUE);
+			precendence.put(v, null);
+			queue.add(v);
+		}
+		//distance from source to source = 0
+		distances.put(v1, 0);
+		
+		while(!queue.isEmpty()) {
+			Vertex v = queue.poll();
+			if(v == v2) {
+				break;
+			}
+			
+			for (Edge e : g.edgesFromVertex(v)) {
+				Vertex u = e.getOtherVertex(v);
+				int alt = distances.get(v) + e.getCost();
+				if(alt < distances.get(u)) {
+					distances.put(u, alt);
+					precendence.put(u, v);
+				}
+			}
+		}
+		
+		Path p = new Path();
+		while(precendence.get(v2) != null) {
+			p.insertAtBeginning(v2);
+			v2 = precendence.get(v2);
+		}
+		p.insertAtBeginning(v2);
+		return p;
 	}
 }
--- src/ch/hepia/it/cffPathFinder/backend/Path.java
+++ src/ch/hepia/it/cffPathFinder/backend/Path.java
@@ -1,4 +1,22 @@
 package ch.hepia.it.cffPathFinder.backend;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Path {
+
+	private List<Vertex> path;
+	
+	public Path() {
+		this.path = new ArrayList<>();
+	}
+	
+	public void insertAtBeginning(Vertex v) {
+		this.path.add(0, v);
+	}
+	
+	public void insertAtEnd(Vertex v) {
+		this.path.add(v);
+	}
+
 }
