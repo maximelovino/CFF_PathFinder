2904ff13038988264168b4a0989b8d97b18b5182
Lovino Maxime <maximelovino@gmail.com>
2017-03-25 17:23:52 +0100

    Floyd done, closes #9

--- src/ch/hepia/it/cffPathFinder/backend/Floyd.java
+++ src/ch/hepia/it/cffPathFinder/backend/Floyd.java
@@ -1,7 +1,14 @@
 package ch.hepia.it.cffPathFinder.backend;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Floyd implements PathFinder {
 	private static Floyd instance = new Floyd();
+	private int[][] distances;
+	private Vertex[][] precedences;
 
 	private Floyd () {
 
@@ -13,6 +20,52 @@ public class Floyd implements PathFinder {
 
 	@Override
 	public Path shortestPath (Graph g, Vertex v1, Vertex v2) {
-		return null;
+		Map<Vertex, Integer> inverseCorrespondance = new HashMap<>();
+		List<Vertex> vertices = g.getVertices();
+
+		distances = new int[vertices.size()][vertices.size()];
+		precedences = new Vertex[vertices.size()][vertices.size()];
+		for (int i = 0; i < vertices.size(); i++) {
+			for (int j = 0; j < vertices.size(); j++) {
+				distances[i][j] = Integer.MAX_VALUE;
+				precedences[i][j] = null;
+			}
+		}
+
+		for (int i = 0; i < vertices.size(); i++) {
+			inverseCorrespondance.put(vertices.get(i), i);
+			distances[i][i] = 0;
+		}
+		for (Edge e : g.getEdges()) {
+			int vTemp1 = inverseCorrespondance.get(e.getV1());
+			int vTemp2 = inverseCorrespondance.get(e.getV2());
+			distances[vTemp1][vTemp2] = e.getCost();
+			distances[vTemp2][vTemp1] = e.getCost();
+			precedences[vTemp1][vTemp2] = e.getV2();
+			precedences[vTemp2][vTemp1] = e.getV1();
+		}
+
+		for (int i = 0; i < vertices.size(); i++) {
+			for (int j = 0; j < vertices.size(); j++) {
+				for (int k = 0; k < vertices.size(); k++) {
+					if (!(distances[i][k] == Integer.MAX_VALUE || distances[k][j] == Integer.MAX_VALUE)) {
+						if (distances[i][j] > distances[i][k] + distances[k][j]) {
+							distances[i][j] = distances[i][k] + distances[k][j];
+							distances[j][i] = distances[i][k] + distances[k][j];
+							precedences[i][j] = precedences[i][k];
+							precedences[j][i] = precedences[j][k];
+						}
+					}
+				}
+			}
+		}
+		Path p = new Path();
+		int cost = distances[inverseCorrespondance.get(v1)][inverseCorrespondance.get(v2)];
+		p.setCost(cost);
+		while (v2 != null) {
+			p.insertAtBeginning(v2);
+			v2 = precedences[inverseCorrespondance.get(v2)][inverseCorrespondance.get(v1)];
+		}
+		return p;
 	}
 }
--- src/ch/hepia/it/cffPathFinder/tests/TestOutput.java
+++ src/ch/hepia/it/cffPathFinder/tests/TestOutput.java
@@ -1,6 +1,9 @@
 package ch.hepia.it.cffPathFinder.tests;
 
+import ch.hepia.it.cffPathFinder.backend.Dijkstra;
+import ch.hepia.it.cffPathFinder.backend.Floyd;
 import ch.hepia.it.cffPathFinder.backend.Graph;
+import ch.hepia.it.cffPathFinder.backend.Path;
 import ch.hepia.it.cffPathFinder.data.XMLTools;
 import org.xml.sax.SAXException;
 
@@ -17,12 +20,22 @@ public class TestOutput {
 		System.out.println(g.adjacencyListString());
 		System.out.println("-----------MATRIX-----------");
 		System.out.println(g.adjacencyMatrixString());
-		System.out.println("-----------XML Output-----------");
-		try {
-			XMLTools.writeToXML("data/out.xml", g);
-		} catch (TransformerException e) {
-			e.printStackTrace();
-		}
+//		System.out.println("-----------XML Output-----------");
+//		try {
+//			XMLTools.writeToXML("data/out.xml", g);
+//		} catch (TransformerException e) {
+//			e.printStackTrace();
+//		}
+		System.out.println("-----------FLOYD-----------");
+		Path floyd = Floyd.getInstance().shortestPath(g, g.getVertex("Sion"), g.getVertex("Zurich"));
+		System.out.println(floyd);
+		System.out.println("Cost " + floyd.getCost());
+		System.out.println("-----------Dijkstra-----------");
+		Path dijkstra = Dijkstra.getInstance().shortestPath(g, g.getVertex("Sion"), g.getVertex("Zurich"));
+		System.out.println(dijkstra);
+		System.out.println("Cost " + dijkstra.getCost());
+
+		System.out.println(g.edgesFromVertex(g.getVertex("Zurich")));
 
 	}
 }
